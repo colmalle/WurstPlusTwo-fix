6ed00a0620a211e887107bb3d426f3822ae604b2
colmalle <unconfigured@null.spigotmc.org>
2020-11-14 10:49:51 +0200

    Fix for Always Night glitching.

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/render/WurstplusAlwaysNight.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/render/WurstplusAlwaysNight.java
@@ -1,10 +1,12 @@
 package me.travis.wurstplus.wurstplustwo.hacks.render;
 
+import me.travis.wurstplus.wurstplustwo.event.events.WurstplusEventPacket;
 import me.travis.wurstplus.wurstplustwo.event.events.WurstplusEventRender;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusCategory;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusHack;
 import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
+import net.minecraft.network.play.server.SPacketTimeUpdate;
 
 public class WurstplusAlwaysNight extends WurstplusHack {
 
@@ -15,7 +17,16 @@ public class WurstplusAlwaysNight extends WurstplusHack {
         this.tag = "AlwaysNight";
         this.description = "see even less";
     }
-
+    
+    @EventHandler
+    private Listener<WurstplusEventPacket.ReceivePacket> on_packet = new Listener<>(event -> {
+        if (event.get_packet() instanceof SPacketTimeUpdate) {
+        	SPacketTimeUpdate packet = (SPacketTimeUpdate) event.get_packet();
+        	mc.world.setTotalWorldTime(packet.getTotalWorldTime());
+        	event.cancel();
+        }
+    });
+    
     @EventHandler
     private Listener<WurstplusEventRender> on_render = new Listener<>(event -> {
         if (mc.world == null) return;
